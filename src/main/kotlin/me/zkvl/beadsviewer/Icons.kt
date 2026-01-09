23129b7593ec18138d37567c2cd7e96df0c51fae
Zakhar Kaval <zakhar.koval@jetbrains.com>
2026-01-09 20:08:22 +0100

    Replace tool window icon with a plugin-specific custom icon.

---
+++ src/main/kotlin/me/zkvl/beadsviewer/Icons.kt
@@ -0,0 +1,9 @@
+package me.zkvl.beadsviewer
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object Icons {
+    @JvmField
+    val PluginIcon: Icon = IconLoader.getIcon("/META-INF/pluginIcon.svg", Icons::class.java)
+}
